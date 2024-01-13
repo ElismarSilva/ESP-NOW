9663d095a26390fcc9d45113293186d015554d69
ElismarSilva <elismar.timaosilva@gmail.com>
2024-01-13 11:30:57 -0300

    Primeiro commit

---
+++ Emissor_Now/Emissor_carrinho_Now.ino
@@ -0,0 +1,69 @@
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+
+uint8_t broadcastAddress[] = {0x08,0x3A,0x8D,0xD3,0x6D,0x39};
+
+//08:3A:8D:D3:6D:39
+typedef struct struct_message {
+  char command; // Comando do botão pressionado ('F', 'E', 'D', 'T', 'P')
+} struct_message;
+
+#define pino_botao_F D2
+#define pino_botao_E D3
+#define pino_botao_D D4
+#define pino_botao_T D5
+#define pino_botao_P D6
+
+struct_message myData;
+
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  Serial.print("Status dos dados enviados: ");
+  if (sendStatus == 0) {
+    Serial.println("Envio ok!");
+  } else {
+    Serial.println("Falha no envio!");
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(pino_botao_F, INPUT_PULLUP);
+  pinMode(pino_botao_E, INPUT_PULLUP);
+  pinMode(pino_botao_D, INPUT_PULLUP);
+  pinMode(pino_botao_T, INPUT_PULLUP);
+  pinMode(pino_botao_P, INPUT_PULLUP);
+
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != 0) {
+    Serial.println("Erro ao inicializar o ESP-NOW");
+    return;
+  }
+
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  esp_now_register_send_cb(OnDataSent);
+
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+}
+
+void loop() {
+  if (!digitalRead(pino_botao_F) == HIGH) {
+    myData.command = 'F';
+  } else if (!digitalRead(pino_botao_E) == HIGH) {
+    myData.command = 'E';
+  } else if (!digitalRead(pino_botao_D) == HIGH) {
+    myData.command = 'D';
+  } else if (!digitalRead(pino_botao_T) == HIGH) {
+    myData.command = 'T';
+  } else if (!digitalRead(pino_botao_P) == HIGH) {
+    myData.command = 'P';
+  } else {
+    // Nenhum botão pressionado, envia um comando de parar por padrão
+    myData.command = 'P';
+  }
+
+  esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+
+  delay(50); // Adicione um pequeno atraso entre leituras para evitar leituras falsas devido a oscilações nos botões
+}
---
+++ Receptor_Now/Receptor_carrinho_Now.ino
@@ -0,0 +1,69 @@
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+
+typedef struct struct_message {
+  char command; // Comando do botão pressionado ('F', 'E', 'D', 'T', 'P')
+} struct_message;
+
+#define pino_led 2
+
+struct_message myData;
+
+void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+
+  Serial.println();
+  Serial.print("Bytes recebidos: ");
+  Serial.println(len);
+  Serial.print("Comando recebido: ");
+  Serial.println(myData.command);
+
+  // Executa ação com base no comando recebido
+  switch (myData.command) {
+    case 'F':
+      digitalWrite(pino_led,0);
+      Serial.println("Recebido comando: Frente!");
+      // Adicione a lógica para avançar
+      break;
+    case 'E':
+      Serial.println("Recebido comando: Esquerda!");
+       //digitalWrite(pino_led,1);
+      // Adicione a lógica para girar à esquerda
+      break;
+    case 'D':
+      Serial.println("Recebido comando: Direita!");
+      // Adicione a lógica para girar à direita
+      break;
+    case 'T':
+      Serial.println("Recebido comando: Trás!");
+      // Adicione a lógica para recuar
+      break;
+    case 'P':
+      Serial.println("Recebido comando: Parado!");
+      digitalWrite(pino_led,1);
+      // Adicione a lógica para parar
+      break;
+    default:
+      Serial.println("Comando desconhecido!");
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(pino_led, OUTPUT);
+  digitalWrite(pino_led, HIGH);
+
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != 0) {
+    Serial.println("Erro ao inicializar o ESP-NOW");
+    return;
+  }
+  esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+void loop() {
+  // Neste programa, não temos comandos no loop
+}
